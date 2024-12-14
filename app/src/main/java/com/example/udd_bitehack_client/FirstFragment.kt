38bf36b38316bc7304ea19f61057aaa8d32c9032
NoMariusz <mariusz2727@vp.pl>
2024-12-14 18:16:51 +0100

    simple main fragment view

--- app/src/main/java/com/example/udd_bitehack_client/FirstFragment.kt
+++ app/src/main/java/com/example/udd_bitehack_client/FirstFragment.kt
@@ -1,6 +1,7 @@
 package com.example.udd_bitehack_client
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,7 +13,7 @@ import com.example.udd_bitehack_client.databinding.FragmentFirstBinding
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
-
+    private val tag = "FirstFragment"
     private var _binding: FragmentFirstBinding? = null
 
     // This property is only valid between onCreateView and
@@ -32,8 +33,13 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        binding.recordBtn.setOnClickListener {
+            Log.d(tag, "button record clicked")
+        }
+        binding.submitBtn.setOnClickListener {
+            val text = binding.textInput.text.toString()
+            Log.d(tag, "button submit clicked, text: $text")
+            binding.textInput.text.clear()
         }
     }
 
